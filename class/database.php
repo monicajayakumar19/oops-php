c2b26e9ddebb699b2fbaea27df9f26120651d488
monicajayakumar19 <61737600+monicajayakumar19@users.noreply.github.com>
2020-03-03 17:58:45 +0530

    Create database.php

---
+++ class/database.php
@@ -0,0 +1,26 @@
+<?php
+class Database(){
+    public $con;
+    public $rows;
+    public $result;
+
+   
+    public $hostname;
+    public $username;
+    public $password;
+    public $db_name;
+    
+}
+public class Database(){
+    $this->hostname ="localhost";
+    $this->username ="root";
+    $this->password="";
+    $this->db_name ="users";
+
+
+}
+public class Connect(){
+    $this->con=mysqli_connect( $this->hostname,$this->username,$this->password) or die("Connection Failed.....");
+    mysqli_select_db_name($this->$con,$this->$result)
+}
+?>
