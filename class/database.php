cc56699ddf5835b6371fbe914166e5dd1d9f164f
monicajayakumar19 <61737600+monicajayakumar19@users.noreply.github.com>
2020-03-05 12:59:16 +0530

    Update database.php

--- class/database.php
+++ class/database.php
@@ -8,25 +8,25 @@ class Database {
     public $hostname;
     public $username;
     public $password;
-    public $db_name;
+    public $dbname;
    
 public function Database() {
     $this->hostname ="localhost";
     $this->username ="root";
     $this->password="";
-    $this->db_name ="users";
+    $this->dbname ="users";
 
 
 }
 public function Connect(){
     $this->con=mysqli_connect( $this->hostname,$this->username,$this->password) or die("Connection Failed.....");
-    mysqli_select_db_name($this->$con,$this->$result) or die("example");
+    mysqli_select_db($this->con,$this->dbname) or die("Database does not exists....");
 
 }
 public function Query($qry){
     $this->Connect();
     $this->result = mysqli_query($this->con,$qry);
-    return $this->$result;
+    return $this->result;
     
 }
 }
