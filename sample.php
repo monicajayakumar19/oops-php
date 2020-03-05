fee23aa1814133f1d4b3694b92d86f6b10f3a443
monicajayakumar19 <61737600+monicajayakumar19@users.noreply.github.com>
2020-03-05 12:28:06 +0530

    Create sample.php

---
+++ sample.php
@@ -0,0 +1,11 @@
+
+<?php
+require_once('class/database.php');
+
+$db = new Database();
+
+$res = $db->Query("select * from employee");
+
+while($rows = mysqli_fetch_object($res)){
+echo $rows->emp_name."</br>";
+}
