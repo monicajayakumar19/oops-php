496247a4d5ecaf5e6417928675d6c6c147beb599
monicajayakumar19 <61737600+monicajayakumar19@users.noreply.github.com>
2020-03-06 12:27:24 +0530

    Update sample.php

--- sample.php
+++ sample.php
@@ -6,6 +6,49 @@ $db = new Database();
 
 $res = $db->Query("select * from employee");
 
-while($rows = mysqli_fetch_object($res)){
-echo $rows->emp_name."</br></br>";
-}
+// while($rows = mysqli_fetch_object($res)){
+// echo $rows->emp_id."</br></br>";
+// echo $rows->emp_name."</br></br>";
+// echo $rows->email."</br></br>";
+// echo $rows->Designation."</br></br>";
+
+// }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <table class="table table-bordered ">
+                    <tr>
+                        <th> EMPLOYEE ID </th>
+                        <th> EMPLOYEE NAME </th>
+                        <th> EMAIL-ID </th>
+                        <th> DESIGNATION </th>
+                    </tr>
+                    <?php while($rows = mysqli_fetch_object($res)) { ?>
+<tr>
+
+    <td><?php echo  $rows->emp_id?></td>
+    <td><?php echo  $rows->emp_name?></td>
+    <td><?php echo  $rows->email?></td>
+    <td><?php echo  $rows->Designation ?></td>
+ 
+</tr>
+
+<?php } ?>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
